d7a46252367449dbcaf7e00ef81d0f0d6301a599
aqwe <meaqwe@hotmail.com>
2025-03-19 02:14:46 +0300

    Production ready

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('aqwe_app.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
     path('stripe/', include('djstripe.urls', namespace='djstripe')),
 ]
 """
