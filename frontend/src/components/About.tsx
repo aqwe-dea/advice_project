f259b6d5fd137d7bf2d59449cb86a5c1d739e0f1
aqwe <meaqwe@hotmail.com>
2025-03-24 01:15:18 +0300

    Production ready

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 function About() {
     return (
         <div className="about-page">
-            <h1>О проекте "Советница АКВИ"</h1>
+            <h1>О проекте Советница АКВИ</h1>
             <p>
                 Это приложение помогает пользователям получать персонализированные советы по различным категориям:
                 финансы, здоровье, образование и другим.
             </p>
-            <img src="/logo.png" alt="Логотип Советницы АКВИ" className="about-logo" />
+            <img src="/logo.svg" alt="Логотип Советницы АКВИ" className="about-logo" />
         </div>
     );
 }
