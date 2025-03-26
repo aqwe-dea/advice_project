3fb1721c5f0e78ae1de938e0bf0a4b4370855828
aqwe <meaqwe@hotmail.com>
2025-03-26 05:46:43 +0300

    Production ready

--- backend/settings.py
+++ backend/settings.py
@@ -130,7 +130,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles/static',
 ]
 
 # Default primary key field type
