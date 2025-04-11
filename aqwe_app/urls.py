6d647707e666541202621069ee6a7d5bf8017c65
aqwe <meaqwe@hotmail.com>
2025-04-11 05:43:51 +0300

    Update app

--- aqwe_app/urls.py
+++ aqwe_app/urls.py
@@ -9,7 +9,12 @@ router.register(r'advice', AdviceViewSet)
 router.register(r'user-history', UserHistoryViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('advice/<int:pk>/', AdviceViewSet.as_view({'get': 'retrieve'}), name='advice-detail'),
-    path('create-payment-intent/', CreatePaymentIntentView.as_view(), name='create_payment_intent'),
+    path('',
+     include(router.urls)),
+    path('advice/<int:pk>/',
+     AdviceViewSet.as_view({'get': 'retrieve'}), name='advice-detail'),
+    path('create-payment-intent/',
+     CreatePaymentIntentView.as_view(), name='create_payment_intent'),
+    path('create-detailed-advice/',
+     CreateDetailedAdviceView.as_view(), name='create_detailed_advice'),
 ]
