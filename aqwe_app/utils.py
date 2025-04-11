131fae71b948ecb0194b21b17469ddb816344106
aqwe <meaqwe@hotmail.com>
2025-04-11 22:02:08 +0300

    Update app

--- aqwe_app/utils.py
+++ aqwe_app/utils.py
@@ -5,24 +5,23 @@ def send_advice_email(user_email: str, advice: Advice):
      subject = 'Ваш детальный совет от АКВИ'
      message = f'категория: {advice.category}\n\nВопрос: {advice.question}\n\nОтвет: {advice.answer}\n\nЗаметки: {advice.notes}'
      with open('advice.txt', 'w') as f:
-      f.write(message)
-     doc = Document()
-     doc.add_heading('Ваш детальный совет от АКВИ', level=1)
-     doc.add_paragraph(f'Категория: {advice.category}')
-     doc.add_paragraph(f'Вопрос: {advice.question}')
-     doc.add_paragraph(f'Ответ: {advice.answer}')
-     doc.add_paragraph(f'Заметки: {advice.notes}')
-     doc.save('advice.doc')
-    email = EmailMessage(
-         subject = subject,
-         body = message,
-         from_email = DEFAULT_FROM_EMAIL,
-         to = [user_email],
-    )
-    email.attach_file('advice.txt')
-    email.attach_file('advice.doc')
-    email.send(fail_silently=False)
-       
-     subject = 'Ваш совет от АКВИ'
-     message = f'Вот ваш совет:\n\n{advice}'
-     send_mail(subject, message, DEFAULT_FROM_EMAIL, [user_email], fail_silently=False)
+          f.write(message)
+          doc = Document()
+          doc.add_heading('Ваш детальный совет от АКВИ', level=1)
+          doc.add_paragraph(f'Категория: {advice.category}')
+          doc.add_paragraph(f'Вопрос: {advice.question}')
+          doc.add_paragraph(f'Ответ: {advice.answer}')
+          doc.add_paragraph(f'Заметки: {advice.notes}')
+          doc.save('advice.doc')
+          email = EmailMessage(
+               subject = subject,
+               body = message,
+               from_email = DEFAULT_FROM_EMAIL,
+               to = [user_email],
+               )
+               email.attach_file('advice.txt')
+               email.attach_file('advice.doc')
+               email.send(fail_silently=False)
+               subject = 'Ваш совет от АКВИ'
+               message = f'Вот ваш совет:\n\n{advice}'
+               send_mail(subject, message, DEFAULT_FROM_EMAIL, [user_email], fail_silently=False)
--- aqwe_app/views.py
+++ aqwe_app/views.py
@@ -1,18 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Advice
-from .serializers import AdviceSerializer
-from .utils import send_advice_email
 from rest_framework import viewsets
 from rest_framework.viewsets import ViewSet
-from .models import UserHistory
-from .serializers import UserHistorySerializer    
 from djstripe.models.core import PaymentIntent
 from djstripe.models import core
 from djstripe.models import PaymentIntent        
 from django.http.response import HttpResponse
 from django.http import response
+from .models import Advice
+from .serializers import AdviceSerializer
+from .utils import send_advice_email
+from .models import UserHistory
+from .serializers import UserHistorySerializer    
 
 class CreateDetailedAdviceView(APIView):
      def post(self, request, *args, **kwargs):
