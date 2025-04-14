05ad44ba39c6a87c3263436e634e5d3cb87a9c3a
aqwe <meaqwe@hotmail.com>
2025-04-15 02:55:54 +0300

    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,50 +7,36 @@ import Home from './components/Home';
 import About from './components/About';
 import Sovet from './components/Sovet';
 import DetailedAdviceForm from './components/DetailedAdviceForm';
-import SovetResult from './components/SovetResult';
-import StripeDonation from './components/StripeDonation';
-//import Notification from './components/Notification';
 import AdviceList from './components/AdviceList';
 import AdviceForm from './components/AdviceForm';
 import Navbar from './components/Navbar';
 
 function App() {
   const { theme, toggleTheme } = useTheme();
-
-  //const [notificationMessage, setNotificationMessage] = useState<string>('');
-
-  //const showNotification = (message: string) => {
-     //setNotificationMessage(message);
-     //setTimeout(() => setNotificationMessage(''), 3000);
-  //};
- 
   return (
     <div className={`App ${theme}`}>
       <Router>
         <Navbar />
         <header className="App-header">
           <h1>Советница АКВИ</h1>
-        <button onClick={toggleTheme}>
-          {theme === 'light' ? 'Переключить на темную тему' : 'Переключить на светлую тему'}
-        </button>
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/sovet" element={<Sovet />} />
-          <Route path="/detailed-advice" element={<DetailedAdviceForm />} />
-          <Route path="/donation" element={<StripeDonation />} />
-          <Route path="/sovet-result/:id" element={<SovetResult />} />
-        </Routes>
-        <AdviceForm />
-        <AdviceList />
-       </main>
-      </Router>
+          <button onClick={toggleTheme}>
+            {theme === 'light' ? 'Переключить на темную тему' : 'Переключить на светлую тему'}
+            </button>
+            <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/sovet" element={<Sovet />} />
+                <Route path="/detailed-advice" element={<DetailedAdviceForm />} />
+                </Routes>
+                <AdviceForm />
+                <AdviceList />
+            </main>
+        </Router>
     </div>
   );
 }
-
 
 export default App;
