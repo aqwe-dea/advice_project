ba623c1878af855532fbdc003f51d235e65c1e33
aqwe <meaqwe@hotmail.com>
2025-05-03 08:24:17 +0300

    Update project

--- aqwe_app/urls.py
+++ aqwe_app/urls.py
@@ -5,19 +5,20 @@ from .views import AdviceViewSet
 from .views import UserHistoryViewSet
 from .views import CreatePaymentIntentView
 from .views import CreateDetailedAdviceView
+from .views import ChatView
 
 router = DefaultRouter()
 router.register(r'advice', AdviceViewSet)
 router.register(r'user-history', UserHistoryViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('advice/<int:pk>/',
-      AdviceViewSet.as_view({'get': 'retrieve'}), name='advice-detail'),
-    path('create-payment-intent/',
-      CreatePaymentIntentView.as_view(), name='create_payment_intent'),
-    path('create-detailed-advice/',
-      CreateDetailedAdviceView.as_view(), name='create_detailed_advice'),
-    path('chat/',
-     ChatView.as_view(), name='chat'),
+  path('', include(router.urls)),
+  path('advice/<int:pk>/', 
+   AdviceViewSet.as_view({'get': 'retrieve'}), name='advice-detail'),
+  path('create-payment-intent/',
+   CreatePaymentIntentView.as_view(), name='create_payment_intent'),
+  path('create-detailed-advice/',
+   CreateDetailedAdviceView.as_view(), name='create_detailed_advice'),
+  path('chat/',
+   ChatView.as_view(), name='chat'),
 ]
