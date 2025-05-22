a883e2305fdaa0edbe31996621a4ef8a4ea792cf
aqwe <meaqwe@hotmail.com>
2025-05-22 07:56:13 +0300

    Big update

--- backend/urls.py
+++ backend/urls.py
@@ -13,6 +13,18 @@ from djstripe.urls import path
 from aqwe_app.views import ChatView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.openapi import Swagger
+from drf_yasg.openapi import Schema
+from drf_yasg.generators import OpenAPISchemaGenerator
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Советница АКВИ API",
+        default_version='v1',
+        description="Документация проекта Советница Акви"
+    ),
+    public=True
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,14 +39,6 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Советница АКВИ API",
-        default_version='v1',
-        description="Документация проекта Советница Акви"
-    ),
-    public=True
-)
 """
 URL configuration for backend project.
 
