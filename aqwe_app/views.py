2a8bde3c0b6417dc69b686cd003a09b32368b091
aqwe <meaqwe@hotmail.com>
2025-07-30 08:11:36 +0300

    Update

--- aqwe_app/views.py
+++ aqwe_app/views.py
@@ -98,6 +98,7 @@ class GenerateCourseView(APIView):
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class LegalDocumentAnalysisView(APIView):
+    parser_classes = [MultiPartParser, FormParser]
     def post(self, request, *args, **kwargs):
         document = request.FILES.get('document')
         country = request.data.get('country', 'Россия')
@@ -129,6 +130,7 @@ class LegalDocumentAnalysisView(APIView):
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class FinancialAnalysisView(APIView):
+    parser_classes = [MultiPartParser, FormParser]
     def post(self, request, *args, **kwargs):
         report = request.FILES.get('report')
         country = request.data.get('country', 'Россия')
