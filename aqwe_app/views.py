538d570abf1c0be79acd1d65be8469c6bae287b8
aqwe <meaqwe@hotmail.com>
2025-09-19 23:35:24 +0300

    Testing fix

--- aqwe_app/views.py
+++ aqwe_app/views.py
@@ -1071,7 +1071,7 @@ class CommunicationOptimizationView(APIView):
             """
             response = client.chat_completion(
                 messages=[{"role": "user", "content": prompt}],
-                max_tokens=700
+                max_tokens=1500
             )
             return Response({
                 'optimization_plan': response.choices[0].message.content,
