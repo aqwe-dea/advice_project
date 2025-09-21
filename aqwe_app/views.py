45b5c310084d7fa9fdf17800eaa96e46843b1dae
aqwe <meaqwe@hotmail.com>
2025-09-21 05:29:00 +0300

    Testing fix

--- aqwe_app/views.py
+++ aqwe_app/views.py
@@ -655,7 +655,7 @@ class PresentationGenerationView(APIView):
             """
             response = client.chat_completion(
                 messages=[{"role": "user", "content": prompt}],
-                max_tokens=2000
+                max_tokens=1800
             )
             return Response({
                 'presentation': response.choices[0].message.content,
